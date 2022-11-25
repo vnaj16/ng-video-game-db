5e196c3b7359c48e668d4f8f053348d628b22b21
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2022-11-25 18:46:15 -0500

    Finish interceptors

---
+++ ng-video-game-db/src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable} from "rxjs";
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+    constructor() {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        req = req.clone({
+            setHeaders: {
+                'x-rapidapi-key': 'TODO: Obtener key de la API DOC',
+                'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com'
+            },
+            setParams: {
+                key: 'TODO: Obtener'
+            }
+        })
+        return next.handle(req)
+    }
+}
--- ng-video-game-db/src/app/model.ts
+++ ng-video-game-db/src/app/model.ts
@@ -13,3 +13,37 @@ export interface Game {
     screenshots: Array<Screenshots>;
     trailers: Array<Trailer>
 }
+
+export interface APIResponse<T> {
+    results: Array<T>;
+}
+
+interface Genre {
+    name: string;
+}
+
+interface ParentPlatform {
+    platform: {
+        name: string;
+    }
+}
+
+interface Publishers {
+    name: string;
+}
+
+interface Rating {
+    id: number;
+    count: number;
+    title: string;
+}
+
+interface Screenshots {
+    image: string
+}
+
+interface Trailer {
+    data: {
+        max: string;
+    };
+}
